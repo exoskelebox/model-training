ed8cca9d46f04424a6bd9dc78039f193e5154f3e
BullDuckling <tbuhla@gmail.com>
2020-04-10 11:58:03 +0200

    PNN rework

--- models/pnn.py
+++ models/pnn.py
@@ -20,49 +20,67 @@ class PNN_Adapter(tf.keras.layers.Layer):
         x = tf.multiply(inputs, self.a)
         x = self.dense(x)
         return x
-        
-class PNN_Model(tf.keras.Model):
-    def __init__(self, layer_info, feature_layer=None, previous=None):
-        super(PNN_Model, self).__init__()
+
+
+class PNN_Column(tf.keras.Model):
+    def __init__(self, layer_info:dict, generation:int=0):
+        super(PNN_Column, self).__init__()
         self._layerinfo=layer_info
-        self.layer_outputs=[None for _ in layer_info]
-        self.previous=previous
-        self.feature_layer = feature_layer
+        self.layer_outputs=[None for _ in layer_info['core']]
+        self.generation = generation
 
     def build(self, input_shape):
-        self.core_layers = [layer_info['type'](units=layer_info['units'], activation=layer_info['activation']) for layer_info in self._layerinfo]
-        if self.previous:
-            all_adapter_layers = []
-            for pretrained in self.previous:
-                pretrained.trainable = False
-                model_adapter_layers = []
-                for layer in self._layerinfo[:-1]: # last layer doesnt get used for new model
-                    adapter = PNN_Adapter()
-                    adapter.build(layer['units'])
-                    model_adapter_layers.append(adapter)
-                if len(model_adapter_layers) >= len(self.core_layers):
-                    print(f"ERROR: too many adapter layers. Needs {len(self.core_layers)-1}, has {len(model_adapter_layers)}")
-                all_adapter_layers.append(model_adapter_layers)
-            self.adapter_layers = all_adapter_layers
-        #self.outputs = [l.output for l in self.core_layers]
+        core_info =  self._layerinfo['core']
+        self.core_layers = [layer_info['type'](
+            units=layer_info['units'], 
+            activation=layer_info['activation']) 
+            for layer_info in core_info]
+
+        adapter_info = self._layerinfo['adapters']
+        self.adapter_layers = [[None for j in range(len(self.core_layers)-1)] 
+        # First layer doesnt get input from pretrained models
+            for i in range(self.generation)]
+        
+        for i in range(self.generation):
+            for j in range(len(self.core_layers)-1):
+                # First layer doesnt get input from pretrained models
+                adapter = adapter_info['type'](
+                    units=adapter_info['units'], 
+                    activation=adapter_info['activation'])
+                adapter.build(core_info[j]['units'])
+                self.adapter_layers[i][j] = adapter
 
     def call(self, inputs):
-        y = inputs
-        pretrained_outputs = []
-        if self.previous:
-            for pretrained in self.previous:
-                pretrained(y)
-                pretrained_outputs.append(pretrained.layer_outputs)
-
-        y = self.feature_layer(y)
+        y = inputs[0]
+        pretrained_outputs = inputs[1:]
         for i in range(len(self.core_layers)):
-            if self.previous and i >= 1: # First layer doesnt get input from pretrained models
-                adapted_pretrained_outputs = [self.adapter_layers[j][i-1](pretrained_outputs[j][i-1]) for j in range(len(pretrained_outputs))]
+            if self.generation >= 1 and i >= 1: 
+                # First layer doesnt get input from pretrained models
+                adapted_pretrained_outputs = [self.adapter_layers[j][i-1](
+                    pretrained_outputs[j][i-1]) for j in range(self.generation)]
                 y = tf.keras.layers.concatenate([y, *adapted_pretrained_outputs])
-
             y = self.core_layers[i](y)
             self.layer_outputs[i] = y
-        return y
+        return self.layer_outputs
+
+
+class PNN_Model(tf.keras.Model):
+    def __init__(self, feature_layer=lambda x: x, columns=[]):
+        super(PNN_Model, self).__init__()
+        self.columns=columns
+        self.feature_layer = feature_layer
+        self.column_outputs = None
+        for column in self.columns[:-1]:
+            # Freeze all but the last column
+            column.trainable = False
+
+    def call(self, inputs):
+        y = [self.feature_layer(inputs)]
+        for column in self.columns:
+            y.append(column(y))
+        self.column_outputs = y
+        return y[-1][-1] # return the output of the last layer of the last column
+        
 
 def test_adapter():
     x = tf.ones((5, 5))
@@ -74,22 +92,41 @@ def test_adapter():
 
 def test_layered():
     x = tf.ones((2, 2))
-    layers = [{'type':tf.keras.layers.Dense, 'units':2, 'activation':'relu'} for i in range(3)]
-    old_model = PNN_Model(layers)
-    old_model.build(x.shape)
-    model = PNN_Model(layers, [old_model])
+    adapters = {
+        'type':tf.keras.layers.Dense, 
+        'units':3, 
+        'activation':'relu'} 
+    core = [{
+        'type':tf.keras.layers.Dense, 
+        'units':5, 
+        'activation':'relu'} 
+        for i in range(3)]
+    layer_info = {'core':core, 'adapters':adapters}
+
+    column_0 = PNN_Column(layer_info)
+    model_0 = PNN_Model(columns=[column_0])
+    model_0.build(x.shape)
+
+
+    column_1 = PNN_Column(layer_info, generation=1)
+    model_1 = PNN_Model(columns=[column_0, column_1])
+    model_1.build(x.shape)
 
     #model.build(x.shape)
     #model.outputs=[l.output for l in model.core_layers]
     print(f"Inputs:\n{x}")
-    y = model(x)
+    y = model_1(x)
     print(f"Output:\n {y.numpy()}")
-    print(f"Outputs:\n {model.layer_outputs}")
+    print(f"Outputs:\n {model_1.column_outputs}")
     #print(f"Weigths:\n {model.weights}")
-    print(f"Summary:")
-    model.summary()    
-    print(f"Summary: Old_Model")
-    old_model.summary()
+    print(f"Summary: model_0")
+    model_0.summary()
+    print(f"Summary: model_1")
+    model_1.summary()    
+    print(f"Summary: column_0")
+    column_0.summary()    
+    print(f"Summary: column_1")
+    column_1.summary()    
 
 if __name__ == "__main__":
     test_layered()
