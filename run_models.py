5b1bd8dfccaabf6d52d7e241c42daf7e2b681e0b
Rógvi Eliasen <rogvieliasen@gmail.com>
2020-04-22 16:25:08 +0200

    support for running multiple models removed tensor warnings added model_config

--- run_models.py
+++ run_models.py
@@ -1,9 +1,20 @@
+import logging
+import os
+logging.getLogger('tensorflow').setLevel(logging.ERROR)
+os.environ["KMP_AFFINITY"] = "noverbose"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import tensorflow as tf
-from models.pnn2 import PNN_Column, PNN_Model
-from models.dense import old_dense_model
+tf.get_logger().setLevel('ERROR')
+tf.autograph.set_verbosity(3)
+
+from models.pnn import PNN_Column, PNN_Model
+from models_config.dense_config import old_dense_model
+from models_config.pnn_config import pnn_config, pnn_model, create_column
+from utils.data_utils import fraction_train_test_split, feature_train_test_split
 from datasets import normalized_human_gestures as human_gestures
 import statistics
-
+import random
 
 feature_layer = human_gestures.get_feature_layer([
     'subject_gender',
@@ -21,6 +32,58 @@ feature_layer = human_gestures.get_feature_layer([
 ])
 
 
+
+def run_models(selected_models = [], reps=5):
+    subjects_accuracy = []
+    columns = []
+    
+    models_config = {
+        'old_dense' : lambda: old_dense_model(),
+        'pnn'   : lambda: pnn_model(*args)
+    }
+    
+    subject_paths = human_gestures.subject_paths
+    random.shuffle(subject_paths)
+
+    for current_model in selected_models:
+        for i in range(len(subject_paths)):
+            k_fold = []
+            print(f"\ntraining on participant: {i}")
+            try:
+                columns.append(create_column(generation_index = i, layer_info=pnn_config()))
+                args = (i, columns)
+                
+                model = models_config[current_model]()
+                
+                for n in range(reps):
+                    train, test = human_gestures.get_data(
+                        human_gestures.subject_paths[i], n, 1024)
+
+                    early_stop_callback = early_stop()
+                    compile_model(model)
+
+                    model.fit(train,
+                                validation_data=test,
+                                epochs=5,
+                                callbacks=[early_stop_callback])
+
+                    result = model.evaluate(test)
+                    print(f"\nModel Evaluated.")
+
+                    k_fold.append(result[-1])
+
+                average = statistics.mean(k_fold)
+                subjects_accuracy.append(average)
+
+            except KeyError:
+                print('Error: Invalid model "%s"' % current_model)
+                continue
+
+        total_average = statistics.mean(subjects_accuracy)
+        print(f"model's average for all participants: {total_average}")
+
+
+
 def compile_model(model):
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
@@ -36,33 +99,5 @@ def early_stop():
 
 
 if __name__ == "__main__":
-    subjects_average = []
-    model = old_dense_model()
-
-    for i in range(len(human_gestures.subject_paths)):
-        k_fold = []
-
-        for n in range(5):
-            train, test = human_gestures.get_data(
-                human_gestures.subject_paths[i], n, 1024)
-
-            early_stop_callback = early_stop()
-            compile_model(model)
-
-            model.fit(train,
-                      validation_data=test,
-                      epochs=5,
-                      callbacks=[early_stop_callback])
-
-            result = model.evaluate(test)
-            print(f"Model Evaluated.")
-
-            k_fold.append(result[-1])
-
-        average = statistics.mean(k_fold)
-        subjects_average.append(average)
-
-        print(f"Average for participant {i} is {average} ")
-
-    total = statistics.mean(subjects_average)
-    print(f"model's average for all participants: {total}")
+    run_models(['pnn','old_dense'])
+    
