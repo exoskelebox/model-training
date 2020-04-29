abfefe68597beff6e651c888e273be19cb20001f
Mikkel Jarlund <mikkel.jarlund@gmail.com>
2020-04-29 11:19:56 +0200

    moved model configs to models

--- run_models.py
+++ run_models.py
@@ -1,29 +1,22 @@
-
+import tensorflow as tf
 import os
 import logging
+from models import Dense
 
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 os.environ["KMP_AFFINITY"] = "noverbose"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-import tensorflow as tf
-import random
-import statistics
-from datasets import normalized_human_gestures as human_gestures
-from utils.data_utils import fraction_train_test_split, feature_train_test_split
-from models_config.pnn_config import PNN
-from models_config.dense_config import old_dense_model
-from models.pnn import PNN_Column, PNN_Model
 tf.get_logger().setLevel('ERROR')
 tf.autograph.set_verbosity(3)
 
 
 def run_models(selected_models=[], reps=5, batch_size=1024, epoch=5):
+
     args = (reps, batch_size, epoch)
 
     models_config = {
-        'old_dense': lambda: old_dense_model(*args),
-        'pnn': lambda: PNN(*args)
+        'dense': lambda: Dense(*args)
     }
 
     for current_model in selected_models:
@@ -36,4 +29,4 @@ def run_models(selected_models=[], reps=5, batch_size=1024, epoch=5):
 
 
 if __name__ == "__main__":
-    run_models(['pnn', 'old_dense'])
+    run_models(['dense'])
