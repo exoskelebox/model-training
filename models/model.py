317df9961ba31552a052605df19c4e18fcb5b5ae
Mikkel Jarlund <mikkel.jarlund@gmail.com>
2020-04-29 12:39:17 +0200

    added logging

--- models/model.py
+++ models/model.py
@@ -8,14 +8,8 @@ class Model:
     def run_model(self):
         raise NotImplementedError("Method must be overridden!")
 
-    def _compile_model(self, model):
-        model.compile(optimizer='adam',
-                      loss='sparse_categorical_crossentropy',
-                      metrics=['accuracy'])
-        print('Model compiled.')
-
     def _early_stop(self):
-        print('Creating callbacks...')
+        print('\nCreating callbacks...')
         return tf.keras.callbacks.EarlyStopping(
             monitor='val_accuracy', min_delta=0.0001,
             patience=3)
--- run_models.py
+++ run_models.py
@@ -1,14 +1,37 @@
-import tensorflow as tf
 import os
 import logging
+
+logging.getLogger('tensorflow').setLevel(logging.ERROR)  # nopep8
+os.environ["KMP_AFFINITY"] = "noverbose"  # nopep8
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # nopep8
+
+import tensorflow as tf
 from models import Dense
 
-logging.getLogger('tensorflow').setLevel(logging.ERROR)
-os.environ["KMP_AFFINITY"] = "noverbose"
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
-tf.get_logger().setLevel('ERROR')
+tf.get_logger().setLevel(logging.ERROR)
 tf.autograph.set_verbosity(3)
+
+# create results logger
+logger = logging.getLogger('results')
+logger.setLevel(logging.INFO)
+
+# create file handler
+fh = logging.FileHandler('results.log')
+fh.setLevel(logging.INFO)
+
+# create console handler
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 
 def run_models(selected_models=[], reps=5, batch_size=1024, epoch=5):
@@ -16,16 +39,16 @@ def run_models(selected_models=[], reps=5, batch_size=1024, epoch=5):
     args = (reps, batch_size, epoch)
 
     models_config = {
-        'dense': lambda: Dense(*args)
+        'dense': Dense
     }
 
     for current_model in selected_models:
 
-        model = models_config[current_model]()
+        model = models_config[current_model](*args)
 
         results = model.run_model()
 
-        print(results)
+        logger.info((current_model, *results))
 
 
 if __name__ == "__main__":
