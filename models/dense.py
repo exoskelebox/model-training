39141ea7e7e74ed4f6debf68d21f9b20431b8695
Mikkel Jarlund <mikkel.jarlund@gmail.com>
2020-04-30 02:23:58 +0200

    train / test / val

--- models/dense.py
+++ models/dense.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from .model import Model
-from datasets import human_gestures
+from datasets import normalized_human_gestures as human_gestures
 import random
 from statistics import mean
 import tensorflow as tf
@@ -17,7 +17,6 @@ class Dense(Model):
 
     def run_model(self):
         subjects_accuracy = []
-        random.shuffle(self.subject_paths)
 
         for i in range(len(self.subject_paths)):
             k_fold = []
@@ -27,19 +26,19 @@ class Dense(Model):
             for n in range(self.reps):
                 model = self._model()
 
-                train, test = human_gestures.get_data(
+                train, val, test = human_gestures.get_data(
                     human_gestures.subject_paths[i], n, self.batch_size)
 
                 early_stop = tf.keras.callbacks.EarlyStopping(
-                    monitor='val_accuracy', min_delta=0.0001,
-                    patience=3)
+                    monitor='val_accuracy', min_delta=0.0001, restore_best_weights=True,
+                    patience=10)
 
                 model.compile(optimizer='adam',
                               loss='sparse_categorical_crossentropy',
                               metrics=['accuracy'])
 
                 model.fit(train,
-                          validation_data=test,
+                          validation_data=val,
                           epochs=self.epoch,
                           callbacks=[early_stop])
 
@@ -47,6 +46,7 @@ class Dense(Model):
                 k_fold.append(result[-1])
 
             average = mean(k_fold)
+            print(f'\nmean accuracy: {average}')
             subjects_accuracy.append(average)
 
         total_average = mean(subjects_accuracy)
