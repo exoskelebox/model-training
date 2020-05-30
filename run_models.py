7ad235bf312285486c16e4d61a4215d2c58e7d57
mjarlu15 <mjarlu15@student.aau.dk@ai-pilot.srv.aau.dk>
2020-05-30 23:38:35 +0200

    lock and loaded

--- run_models.py
+++ run_models.py
@@ -45,4 +45,4 @@ def run_models(selected_models=[], batch_size=1024, epochs=5):
 
 
 if __name__ == "__main__":
-    run_models(['pnn'], 2**10, 100)
+    run_models(['cpnn'], 2**9, 100)
--- test.py
+++
@@ -1,5 +0,0 @@
-from models.pnn_test import PNN
-from kerastuner import HyperParameters
-
-pnn = PNN()
-pnn.fit()
